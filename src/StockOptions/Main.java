965de3221191cb980bf1e39490af74017fbcd623
Harnish Shah <hrsh6845@gmail.com>
2024-06-29 10:07:49 -0400

    added the initial test.

---
+++ src/StockOptions/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        var market = new MarketDataApi();
+
+        System.out.println(market.Option);
+    }
+}
---
+++ src/StockOptionsTest/Options/OptionTest.java
@@ -0,0 +1,5 @@
+package StockOptionsTest.Options;
+
+class OptionTest {
+    
+}
